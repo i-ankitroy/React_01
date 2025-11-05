833c50344bee5e52a3f219d123571e4798e0ebcc
Ankit Roy <ankitkumarroy48@gmail.com>
2025-11-05 23:03:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+const App = () => {
+  return (
+    <>
+     <div id="dad">
+      <h1>hello Guys how are u!!</h1>
+      <h2>I am Ankit!!</h2>
+    </div>
+    <div id="chacha">
+      <p>Kya Kaise Ho!</p>
+    </div>
+    </>
+  )
+}
+
+export default App
